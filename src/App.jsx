edebdbff2c593e9984bc80c4b27d175b8a20b05b
Fady-Alfred <fadyalfred77@gmail.com>
2026-05-01 18:33:30 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-generalsans text-black-100 bg-terminal">
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
