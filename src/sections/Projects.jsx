5b09fad0a855b35ac6b0d658774cd392a02a2bbf
Fady-Alfred <fadyalfred77@gmail.com>
2026-05-16 16:26:21 +0300

    feat: improve layout of Projects section by adding gap between tags

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -37,7 +37,7 @@ const Projects = () => {
                         <p>{currentProject.subdesc}</p>
                     </div>
                     <div className={"flex items-center justify-between flex-wrap gap-5"}>
-                        <div className={"flex items-center"}>
+                        <div className={"flex items-center gap-2"}>
                             {currentProject.tags.map((tag,index) => (
                                 <div key={index} className={"tech-logo"}>
                                     <img src={tag.path} alt={tag.name}/>
